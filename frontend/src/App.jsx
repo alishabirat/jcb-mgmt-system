c0162fb2d98a3b7a71b5e21f3c97b094858ca9fe
Diwas poudel <diwaslearn@gmail.com>
2025-03-15 20:39:01 +0545

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,21 @@ import Customers from "./pages/Customers";
 import Trips from "./pages/Trips";
 import Expenses from "./pages/Expenses";
 import Navbar from "./components/Navbar";
-
-
 export default function App() {
   return (
     <Router>
-     <div className="sticky z-10 top-0 left-0 right-0 bottom-0">
+      <div className="sticky z-10 top-0 left-0 right-0 bottom-0">
         <Navbar />
       </div>
       <div className="flex h-auto top-0 left-0 right-0 bottom-0">
         <Sidebar />
-        <div className="ml-2 flex-1 p-6"> {/* Push content to the right */}
+        <div className="ml-2 flex-1 p-6">
+          {/* Push content to the right */}
           <Routes>
             <Route path="/" element={<JcbDashboard />} />
             <Route path="/customers" element={<Customers />} />
             <Route path="/payments" element={<Payments />} />
-            <Route path="/trips" element={<Trips />} /> 
+            <Route path="/trips" element={<Trips />} />
             <Route path="/expenses" element={<Expenses />} />
           </Routes>
         </div>
